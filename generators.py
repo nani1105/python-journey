d34f9ed92a93e81c807472ed007dd0ad912a669a
Nani <girigedupuri@gmail.com>
2026-05-04 17:25:29 +0530

    day 3: Generators, Yield

---
+++ generators.py
@@ -0,0 +1,20 @@
+num = [1,2,3,4,5,6,7,8,9]
+squares=[n**2 for n in num]
+evens=[n for n in num if n%2==0 ]
+squares_even =[n**2 for n in num if n%2==0]
+print(squares)
+print(evens)
+print(squares_even)
+ 
+def fibonacci():
+    a,b=0,1
+    while True:
+        yield a
+        a,b=b, a+b   
+gen= fibonacci()
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
